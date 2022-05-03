19a4421661345696a5bcbbf68f1b72d29eb9163f
The-Six <ijazdriscoll@gmail.com>
2022-05-03 16:20:29 +0100

    We have created a seperation of concerns and created a function to render the title

---
+++ src/Components/NavBar/navBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from './title'
+
+function NavBar () {
+    return (
+        <div>
+        <Title/>
+            
+        </div>
+    )
+}
+
+export default NavBar
---
+++ src/Components/NavBar/title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <>
+        <h1>
+        House of Heroes  
+        </h1>
+        </>
+    );
+}
+
+export default Title
