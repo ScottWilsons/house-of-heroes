fc0c8eb1fc1f30b1a520c0d6213e21bffcd2759d
waynethrower <wayne.thrower@hotmail.co.uk>
2022-05-11 12:07:54 +0100

    Added Play/play again button, refactored initialising round

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import React from "react";
-import "./App.css";
-import NavBar from "./components/NavBar/navBar";
-import { useState, useEffect } from "react";
-import HeroCard from "./components/Card/HeroCard.js";
+import React from "react"
+import "./App.css"
+import NavBar from "./components/NavBar/navBar"
+import { useState, useEffect } from "react"
+import HeroCard from "./components/Card/HeroCard.js"
 
-let randomNumber = Math.floor(Math.random() * 564);
-let randomNumber2 = Math.floor(Math.random() * 564);
+// let randomNumber = Math.floor(Math.random() * 564)
+// let randomNumber2 = Math.floor(Math.random() * 564)
 
 function App() {
-  const [heros, setheros] = useState("");
-  const [playerScore, setplayerScore] = useState(0);
-  const [computerScore, setcomputerScore] = useState(0);
+  const [heros, setheros] = useState("")
+  const [playerScore, setplayerScore] = useState(0)
+  const [computerScore, setcomputerScore] = useState(0)
+  const [playerCard, setPlayerCard] = useState()
+  const [computerCard, setComputerCard] = useState()
+  const [playButton, setPlayButton] = useState(false)
 
   useEffect(() => {
     async function FetchSuperHero() {
       const response = await fetch(
         `https://akabab.github.io/superhero-api/api/all.json`
-      );
-      const data = await response.json();
-      setheros(data);
+      )
+      const data = await response.json()
+      setheros(data)
     }
-    FetchSuperHero();
-  }, []);
+    FetchSuperHero()
+  }, [])
 
-  const playerHero = heros[randomNumber];
-  const computerHero = heros[randomNumber2];
+  // const playerHero = heros[randomNumber]
+  // const computerHero = heros[randomNumber2]
 
   function theBattle(selectedStat) {
     if (
-      playerHero.powerstats[selectedStat] ===
-      computerHero.powerstats[selectedStat]
+      playerCard.powerstats[selectedStat] ===
+      computerCard.powerstats[selectedStat]
     ) {
-      console.log("Draw");
+      console.log("Draw")
     } else if (
-      playerHero.powerstats[selectedStat] <
-      computerHero.powerstats[selectedStat]
+      playerCard.powerstats[selectedStat] <
+      computerCard.powerstats[selectedStat]
     ) {
-      console.log("Lose");
-      setcomputerScore(+1);
+      console.log("Lose")
+      setcomputerScore(computerScore + 1)
     } else if (
-      playerHero.powerstats[selectedStat] >
-      computerHero.powerstats[selectedStat]
+      playerCard.powerstats[selectedStat] >
+      computerCard.powerstats[selectedStat]
     ) {
-      console.log("Win");
-      setplayerScore(+1);
+      console.log("Win")
+      setplayerScore(playerScore + 1)
     }
+  }
+
+  function initRound() {
+    setPlayButton(true)
+    let randomNumber = Math.floor(Math.random() * 564)
+    let randomNumber2 = Math.floor(Math.random() * 564)
+    const playerHero = heros[randomNumber]
+    setPlayerCard(playerHero)
+    const computerHero = heros[randomNumber2]
+    setComputerCard(computerHero)
   }
 
   return heros ? (
@@ -56,15 +69,18 @@ function App() {
       <button onClick={() => theBattle("power")}>Power</button>
       <button onClick={() => theBattle("speed")}>Speed</button>
       <button onClick={() => theBattle("strength")}>Strength</button>
-      <HeroCard heros={computerHero} />
-      <HeroCard heros={playerHero} />
+      {computerCard ? <HeroCard heros={computerCard} /> : <></>}
+      {playerCard ? <HeroCard heros={playerCard} /> : <></>}
+      <button onClick={() => initRound()}>
+        {playButton ? "Play Again" : "Play"}
+      </button>
     </>
   ) : (
     <>
       <NavBar />
       <>Loading...</>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
