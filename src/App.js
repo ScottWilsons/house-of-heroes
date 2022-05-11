b2e77783400ca0cc70e4279437c6d5864a1b9901
Scott Wilson <swrs@hotmail.co.uk>
2022-05-11 12:42:11 +0100

    disabled the buttons after the selected stat is chosen

--- src/App.js
+++ src/App.js
@@ -1,74 +1,92 @@
-import React from "react"
-import "./App.css"
-import NavBar from "./components/NavBar/navBar"
-import { useState, useEffect } from "react"
-import HeroCard from "./components/Card/HeroCard.js"
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/navBar";
+import { useState, useEffect } from "react";
+import HeroCard from "./components/Card/HeroCard.js";
 
 // let randomNumber = Math.floor(Math.random() * 564)
 // let randomNumber2 = Math.floor(Math.random() * 564)
 
 function App() {
-  const [heros, setheros] = useState("")
-  const [playerScore, setplayerScore] = useState(0)
-  const [computerScore, setcomputerScore] = useState(0)
-  const [playerCard, setPlayerCard] = useState()
-  const [computerCard, setComputerCard] = useState()
-  const [playButton, setPlayButton] = useState(false)
+  const [heros, setheros] = useState("");
+  const [playerScore, setplayerScore] = useState(0);
+  const [computerScore, setcomputerScore] = useState(0);
+  const [playerCard, setPlayerCard] = useState();
+  const [computerCard, setComputerCard] = useState();
+  const [playButton, setPlayButton] = useState(false);
+  const [disableButton, setDisableButton] = useState(false);
 
   useEffect(() => {
     async function FetchSuperHero() {
       const response = await fetch(
         `https://akabab.github.io/superhero-api/api/all.json`
-      )
-      const data = await response.json()
-      setheros(data)
+      );
+      const data = await response.json();
+      setheros(data);
     }
-    FetchSuperHero()
-  }, [])
+    FetchSuperHero();
+  }, []);
 
   // const playerHero = heros[randomNumber]
   // const computerHero = heros[randomNumber2]
 
   function theBattle(selectedStat) {
+    setDisableButton(true);
     if (
       playerCard.powerstats[selectedStat] ===
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Draw")
+      console.log("Draw");
     } else if (
       playerCard.powerstats[selectedStat] <
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Lose")
-      setcomputerScore(computerScore + 1)
+      console.log("Lose");
+      setcomputerScore(computerScore + 1);
     } else if (
       playerCard.powerstats[selectedStat] >
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Win")
-      setplayerScore(playerScore + 1)
+      console.log("Win");
+      setplayerScore(playerScore + 1);
     }
   }
 
   function initRound() {
-    setPlayButton(true)
-    let randomNumber = Math.floor(Math.random() * 564)
-    let randomNumber2 = Math.floor(Math.random() * 564)
-    const playerHero = heros[randomNumber]
-    setPlayerCard(playerHero)
-    const computerHero = heros[randomNumber2]
-    setComputerCard(computerHero)
+    setDisableButton(false);
+    setPlayButton(true);
+    let randomNumber = Math.floor(Math.random() * 564);
+    let randomNumber2 = Math.floor(Math.random() * 564);
+    const playerHero = heros[randomNumber];
+    setPlayerCard(playerHero);
+    const computerHero = heros[randomNumber2];
+    setComputerCard(computerHero);
   }
 
   return heros ? (
     <>
       <NavBar playerScore={playerScore} computerScore={computerScore} />
-      <button onClick={() => theBattle("combat")}>Combat</button>
-      <button onClick={() => theBattle("durability")}>Durability</button>
-      <button onClick={() => theBattle("intelligence")}>Intelligence</button>
-      <button onClick={() => theBattle("power")}>Power</button>
-      <button onClick={() => theBattle("speed")}>Speed</button>
-      <button onClick={() => theBattle("strength")}>Strength</button>
+      <button disabled={disableButton} onClick={() => theBattle("combat")}>
+        Combat
+      </button>
+      <button disabled={disableButton} onClick={() => theBattle("durability")}>
+        Durability
+      </button>
+      <button
+        disabled={disableButton}
+        onClick={() => theBattle("intelligence")}
+      >
+        Intelligence
+      </button>
+      <button disabled={disableButton} onClick={() => theBattle("power")}>
+        Power
+      </button>
+      <button disabled={disableButton} onClick={() => theBattle("speed")}>
+        Speed
+      </button>
+      <button disabled={disableButton} onClick={() => theBattle("strength")}>
+        Strength
+      </button>
       {computerCard ? <HeroCard heros={computerCard} /> : <></>}
       {playerCard ? <HeroCard heros={playerCard} /> : <></>}
       <button onClick={() => initRound()}>
@@ -80,7 +98,7 @@ function App() {
       <NavBar />
       <>Loading...</>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
