8db2dfddbd566d537a181f78a84f33a3cf418f6b
Scott Wilson <swrs@hotmail.co.uk>
2022-05-23 13:20:39 +0100

    renamed components to main

--- src/App.js
+++ src/App.js
@@ -1,68 +1,68 @@
-import React from "react"
-import { useState, useEffect } from "react"
+import React from "react";
+import { useState, useEffect } from "react";
 
 //components
-import NavBar from "./components/NavBar/navBar"
-import HeroCard from "./components/Card/heroCard.js"
-import PlayerCard from "./components/PlayerCard/playerCard"
+import NavBar from "./Main/NavBar/navBar";
+import HeroCard from "./Main/Card/heroCard.js";
+import PlayerCard from "./Main/PlayerCard/playerCard";
 
 //styling
-import "./style.sass"
+import "./style.sass";
 
 function App() {
-  const [heros, setheros] = useState("")
-  const [playerScore, setplayerScore] = useState(0)
-  const [computerScore, setcomputerScore] = useState(0)
-  const [playerCard, setPlayerCard] = useState()
-  const [computerCard, setComputerCard] = useState()
-  const [playButton, setPlayButton] = useState(false)
-  const [disableButton, setDisableButton] = useState(false)
-  const [playDisabled, setPlayDisabled] = useState(false)
+  const [heros, setheros] = useState("");
+  const [playerScore, setplayerScore] = useState(0);
+  const [computerScore, setcomputerScore] = useState(0);
+  const [playerCard, setPlayerCard] = useState();
+  const [computerCard, setComputerCard] = useState();
+  const [playButton, setPlayButton] = useState(false);
+  const [disableButton, setDisableButton] = useState(false);
+  const [playDisabled, setPlayDisabled] = useState(false);
 
   useEffect(() => {
     async function FetchSuperHero() {
       const response = await fetch(
         `https://akabab.github.io/superhero-api/api/all.json`
-      )
-      const data = await response.json()
-      setheros(data)
+      );
+      const data = await response.json();
+      setheros(data);
     }
-    FetchSuperHero()
-  }, [])
+    FetchSuperHero();
+  }, []);
 
   function theBattle(selectedStat) {
-    setDisableButton(true)
-    setPlayDisabled(false)
+    setDisableButton(true);
+    setPlayDisabled(false);
     if (
       playerCard.powerstats[selectedStat] ===
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Draw")
+      console.log("Draw");
     } else if (
       playerCard.powerstats[selectedStat] <
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Lose")
-      setcomputerScore(computerScore + 1)
+      console.log("Lose");
+      setcomputerScore(computerScore + 1);
     } else if (
       playerCard.powerstats[selectedStat] >
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Win")
-      setplayerScore(playerScore + 1)
+      console.log("Win");
+      setplayerScore(playerScore + 1);
     }
   }
 
   function initRound() {
-    setDisableButton(false)
-    setPlayButton(true)
-    setPlayDisabled(true)
-    let randomNumber = Math.floor(Math.random() * 564)
-    let randomNumber2 = Math.floor(Math.random() * 564)
-    const playerHero = heros[randomNumber]
-    setPlayerCard(playerHero)
-    const computerHero = heros[randomNumber2]
-    setComputerCard(computerHero)
+    setDisableButton(false);
+    setPlayButton(true);
+    setPlayDisabled(true);
+    let randomNumber = Math.floor(Math.random() * 564);
+    let randomNumber2 = Math.floor(Math.random() * 564);
+    const playerHero = heros[randomNumber];
+    setPlayerCard(playerHero);
+    const computerHero = heros[randomNumber2];
+    setComputerCard(computerHero);
   }
 
   return heros ? (
@@ -95,7 +95,7 @@ function App() {
       <NavBar />
       <>Loading...</>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
