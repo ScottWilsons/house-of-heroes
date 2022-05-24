b350fcf3267bdbf00d344ab4f8cabe867549ad31
waynethrower <wayne.thrower@hotmail.co.uk>
2022-05-24 11:38:38 +0100

    Corrected first card not being delayed

--- src/App.js
+++ src/App.js
@@ -1,93 +1,95 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from "react"
+import { useState, useEffect } from "react"
 
 //components
-import NavBar from "./components/NavBar/navBar";
-import HeroCard from "./components/Card/heroCard";
-import PlayerCard from "./components/PlayerCard/playerCard";
+import NavBar from "./components/NavBar/navBar"
+import HeroCard from "./components/Card/heroCard"
+import PlayerCard from "./components/PlayerCard/playerCard"
 
 //styling
-import "./style.sass";
+import "./style.sass"
 
 function App() {
-  const [heros, setheros] = useState("");
-  const [playerScore, setplayerScore] = useState(0);
-  const [computerScore, setcomputerScore] = useState(0);
-  const [playerCard, setPlayerCard] = useState();
-  const [computerCard, setComputerCard] = useState();
-  const [playButton, setPlayButton] = useState(false);
-  const [disableButton, setDisableButton] = useState(false);
-  const [playDisabled, setPlayDisabled] = useState(false);
-  const [style, setStyle] = useState("heroCard-inner");
+  const [heros, setheros] = useState("")
+  const [playerScore, setplayerScore] = useState(0)
+  const [computerScore, setcomputerScore] = useState(0)
+  const [playerCard, setPlayerCard] = useState()
+  const [computerCard, setComputerCard] = useState()
+  const [playButton, setPlayButton] = useState(false)
+  const [disableButton, setDisableButton] = useState(false)
+  const [playDisabled, setPlayDisabled] = useState(false)
+  const [style, setStyle] = useState("heroCard-inner")
 
   const changeStyle = () => {
-    console.log("you just clicked");
+    console.log("you just clicked")
 
-    setStyle("heroCard-transform");
-  };
+    setStyle("heroCard-transform")
+  }
 
   const changeStyle2 = () => {
-    console.log("you just clicked");
+    console.log("you just clicked")
 
-    setStyle("heroCard-inner");
-  };
+    setStyle("heroCard-inner")
+  }
 
   useEffect(() => {
     async function FetchSuperHero() {
       const response = await fetch(
         `https://akabab.github.io/superhero-api/api/all.json`
-      );
-      const data = await response.json();
+      )
+      const data = await response.json()
       // let module = require("./data.js")
       // let data = module.data
       // console.log(data)
-      setheros(data);
+      setheros(data)
     }
-    FetchSuperHero();
-  }, []);
+    FetchSuperHero()
+  }, [])
 
-  console.log(style);
+  console.log(style)
 
   function theBattle(selectedStat) {
-    changeStyle();
-    console.log(style);
-    setDisableButton(true);
-    setPlayDisabled(false);
+    changeStyle()
+    console.log(style)
+    setDisableButton(true)
+    setPlayDisabled(false)
     if (
       playerCard.powerstats[selectedStat] ===
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Draw");
+      console.log("Draw")
     } else if (
       playerCard.powerstats[selectedStat] <
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Lose");
-      setcomputerScore(computerScore + 1);
+      console.log("Lose")
+      setcomputerScore(computerScore + 1)
     } else if (
       playerCard.powerstats[selectedStat] >
       computerCard.powerstats[selectedStat]
     ) {
-      console.log("Win");
-      setplayerScore(playerScore + 1);
+      console.log("Win")
+      setplayerScore(playerScore + 1)
     }
   }
 
   function initRound() {
-    changeStyle2();
-    setDisableButton(false);
-    setPlayButton(true);
-    setPlayDisabled(true);
-    let randomNumber = Math.floor(Math.random() * 564);
-    let randomNumber2 = Math.floor(Math.random() * 564);
-    const playerHero = heros[randomNumber];
-    setPlayerCard(playerHero);
-    const computerHero = heros[randomNumber2];
-    setTimeout(() => {
-      setComputerCard(computerHero);
-    }, 1000);
-    //working out setTimeout to delay getting the next computer card, need to wrok out how to not interfere with loading the first card
-    console.log(computerHero);
+    changeStyle2()
+    setDisableButton(false)
+    setPlayDisabled(true)
+    let randomNumber = Math.floor(Math.random() * 564)
+    let randomNumber2 = Math.floor(Math.random() * 564)
+    const playerHero = heros[randomNumber]
+    setPlayerCard(playerHero)
+    const computerHero = heros[randomNumber2]
+    //check play button to see if first round, first round delay not required, second round timeout required to keep next card information hidden
+    playButton
+      ? setTimeout(() => {
+          setComputerCard(computerHero)
+        }, 1000)
+      : setComputerCard(computerHero)
+    console.log(computerHero)
+    setPlayButton(true)
   }
 
   return heros ? (
@@ -124,7 +126,7 @@ function App() {
       <NavBar />
       <>Loading...</>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
